7b36b9801279557caf1b596df4f1d99f88695f11
2440194388 <75823897+2440194388@users.noreply.github.com>
2021-01-23 11:55:03 +0800

    Update qqreadck.js

--- qqreadck.js
+++ qqreadck.js
@@ -36,7 +36,7 @@ if (
     );
     $.msg(jsname, `qqreadtimehd${jbid}: 获取时长header: 成功🎉`, ``);
   } 
-  else if ($request &&$request.body.indexOf("succ_C")>=0&&$request.body.indexOf("get_C")>=0){
+  if ($request &&$request.body.indexOf("succ_C")>=0&&$request.body.indexOf("get_C")>=0){
     const qqreadbodyVal = $request.body;
     if (qqreadbodyVal) $.setdata(qqreadbodyVal, "qqreadbd"+jbid);
     $.log(
